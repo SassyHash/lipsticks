eed179aed066397e2e7f2bbd02dc2bd8e082ed22
Sassy <b@sassyha.sh>
2013-12-25 19:18:58 -0800

    starting testing

--- public/js/Shades.js
+++ public/js/Shades.js
@@ -1,3 +1,20 @@
 (function($) {
 
+  window.Collection = Backbone.Model.extend({});
+  window.CollectionView = Backbone.View.extend({
+    tagName: "li",
+    className: "collection",
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.bind('change', this.render);
+      this.template = _.template($('#collection-template').html());
+    },
+    render    : function() {
+      var renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent);
+      return this;
+    } 
+
+  });
+
 })(jQuery);
